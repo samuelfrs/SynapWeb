44379c1c3037327eca8853d3c9cffd3bafe726ca
Samuel <samuelgfarias@gmail.com>
2026-09-04 13:00:49 -0300

    chore(backend): load dotenv in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
